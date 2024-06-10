c2e4043c6acb4a7b367a89e4b8f1d720772cf8fe
Azizbek Abdurahmonov <abdurahmonov-azizbek@outlook.com>
2024-06-10 03:59:42 -0700

    INFRA: Initialize Test Project

---
+++ VidSphere.Core.Api.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by VidSphere Team
+// --------------------------------------------------------
+
+namespace VidSphere.Core.Api.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
