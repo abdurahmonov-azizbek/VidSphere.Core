ba47638061fb97c73664dfa06cd43f8d3cb90173
Azizbek Abdurahmonov <abdurahmonov-azizbek@outlook.com>
2024-06-10 04:14:46 -0700

    BROKERS: Add StorageBroker

---
+++ VidSphere.Core.Api/Brokers/Storages/StorageBroker.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using Microsoft.EntityFrameworkCore;
+
+namespace VidSphere.Core.Api.Brokers.Storages
+{
+    public class StorageBroker : DbContext, IStorageBroker
+    {
+        private readonly IConfiguration configuration;
+
+        public StorageBroker(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.Database.Migrate();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = this.configuration.GetConnectionString(name: "DefaultConnection");
+
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+    }
+}
