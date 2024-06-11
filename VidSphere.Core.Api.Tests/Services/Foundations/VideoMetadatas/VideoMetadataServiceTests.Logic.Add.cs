97da824311003cf7fb963c713878850f9cae29e3
Abdurahmonov Azizbek <abdurahmonov-azizbek@outlook.com>
2024-06-11 12:19:03 -0700

    ShouldAddVideoMetadataAsync -> Fail

---
+++ VidSphere.Core.Api.Tests/Services/Foundations/VideoMetadatas/VideoMetadataServiceTests.Logic.Add.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by VidSphere Team
+// --------------------------------------------------------
+
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using VidSphere.Core.Api.Models.VideoMetadatas;
+
+namespace VidSphere.Core.Api.Tests.Services.Foundations.VideoMetadatas
+{
+    public partial class VideoMetadataServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddVideoMetadataAsync()
+        {
+            //given
+            DateTimeOffset randomDate = GetRandomDateTimeOffset();
+            VideoMetadata randomVideoMetadata = CreateRandomVideoMetadata(randomDate);
+            VideoMetadata inputVideoMetadata = randomVideoMetadata;
+            VideoMetadata persistedVideoMetadata = inputVideoMetadata;
+            VideoMetadata expectedVideoMetadata = persistedVideoMetadata.DeepClone();
+
+            this.datetimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffset()).Returns(randomDate);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertVideoMetadataAsync(inputVideoMetadata)).ReturnsAsync(persistedVideoMetadata);
+
+            //when
+            VideoMetadata actualVideoMetadata = await this.videoMetadataService
+                .AddVideoMetadataAsync(inputVideoMetadata);
+
+            //then
+            actualVideoMetadata.Should().BeEquivalentTo(expectedVideoMetadata);
+
+            this.datetimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffset(), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertVideoMetadataAsync(inputVideoMetadata), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.datetimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ VidSphere.Core.Api.Tests/Services/Foundations/VideoMetadatas/VideoMetadataServiceTests.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by VidSphere Team
+// --------------------------------------------------------
+
+using Moq;
+using Tynamix.ObjectFiller;
+using VidSphere.Core.Api.Brokers.DateTimes;
+using VidSphere.Core.Api.Brokers.Loggings;
+using VidSphere.Core.Api.Brokers.Storages;
+using VidSphere.Core.Api.Models.VideoMetadatas;
+
+namespace VidSphere.Core.Api.Tests.Services.Foundations.VideoMetadatas
+{
+    public partial class VideoMetadataServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<IDateTimebroker> datetimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IVideoMetadataService videoMetadataService;
+
+        public VideoMetadataServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.datetimeBrokerMock = new Mock<IDateTimebroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.videoMetadataService = new VideoMetadataService(
+                storageBroker: this.storageBrokerMock.Object,
+                dateTimebroker: this.datetimeBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: DateTime.UnixEpoch).GetValue();
+
+        private static VideoMetadata CreateRandomVideoMetadata(DateTimeOffset dates) =>
+            CreateVideoMetadataFiller(dates).Create();
+
+        private static VideoMetadata CreateRandomVideoMetadata() =>
+            CreateVideoMetadataFiller(GetRandomDateTimeOffset()).Create();
+
+        private static Filler<VideoMetadata> CreateVideoMetadataFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<VideoMetadata>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates);
+
+            return filler;
+        }
+    }
+}
