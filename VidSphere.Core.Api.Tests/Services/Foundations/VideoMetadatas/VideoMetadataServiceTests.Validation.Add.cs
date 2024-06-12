bd491e7527918cf0e95a6cccf7050e7291149831
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-06-12 18:39:45 +0500

    ShouldThrowValidationexceptionOnAddIfVideoMetadataIsNullAndLogErrorAsync -> FAIL

---
+++ VidSphere.Core.Api.Tests/Services/Foundations/VideoMetadatas/VideoMetadataServiceTests.Validation.Add.cs
@@ -0,0 +1,51 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VidSphere.Core.Api.Models.VideoMetadatas.Exceptions;
+using VidSphere.Core.Api.Models.VideoMetadatas;
+using FluentAssertions;
+
+namespace VidSphere.Core.Api.Tests.Services.Foundations.VideoMetadatas
+{
+    public partial class VideoMetadataServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationexceptionOnAddIfVideoMetadataIsNullAndLogErrorAsync()
+        {
+            //given
+            VideoMetadata nullVideoMetadata = null;
+            var nullVideoMetadataException = new NullVideoMetadataException("Video Metadata is null.");
+
+            var expectedvideoMetadataValidationException =
+                new VideoMetadataValidationException(
+                    "Video Metadata Validation Exception occured, fix the errors and try again.",
+                        nullVideoMetadataException);
+
+            //when
+            ValueTask<VideoMetadata> addVideoMetadataTask =
+                this.videoMetadataService.AddVideoMetadataAsync(nullVideoMetadata);
+
+            VideoMetadataValidationException actualVideoMetadataValidationException =
+                await Assert.ThrowsAsync<VideoMetadataValidationException>(() =>
+                    addVideoMetadataTask.AsTask());
+
+            //then
+            actualVideoMetadataValidationException.Should().BeEquivalentTo(expectedvideoMetadataValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedvideoMetadataValidationException))),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertVideoMetadataAsync(It.IsAny<VideoMetadata>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ VidSphere.Core.Api/Models/VideoMetadatas/Exceptions/VideoMetadataValidationException.cs
@@ -0,0 +1,11 @@
+﻿using Xeptions;
+
+namespace VidSphere.Core.Api.Models.VideoMetadatas.Exceptions
+{
+    public class VideoMetadataValidationException : Xeption
+    {
+        public VideoMetadataValidationException(string message, Exception innerException)
+            :base(message, innerException)
+        { }
+    }
+}
