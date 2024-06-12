8290c28a13462cf6e6b24ff89cdcd3c3982df99f
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-06-12 19:13:40 +0500

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> PASS

--- VidSphere.Core.Api/Services/Foundations/VideoMetadatas/VideoMetadataService.Exceptions.cs
+++ VidSphere.Core.Api/Services/Foundations/VideoMetadatas/VideoMetadataService.Exceptions.cs
@@ -1,4 +1,5 @@
-﻿using VidSphere.Core.Api.Models.VideoMetadatas;
+﻿using Microsoft.Data.SqlClient;
+using VidSphere.Core.Api.Models.VideoMetadatas;
 using VidSphere.Core.Api.Models.VideoMetadatas.Exceptions;
 using Xeptions;
 
@@ -22,6 +23,27 @@ namespace VidSphere.Core.Api.Services.Foundations.VideoMetadatas
             {
                 throw CreateAndLogValidationException(invalidVideoMetadataException);
             }
+            catch (SqlException sqlException)
+            {
+                var failedVideoMetadataStorageException =
+                    new FailedVideoMetadataStorageException(
+                        "Failed Video Metadata storage error occured, please contact support.",
+                            sqlException);
+
+                throw CreateAndLogCriticalDependencyException(failedVideoMetadataStorageException);
+            }
+        }
+
+        private VideoMetadataDependencyException CreateAndLogCriticalDependencyException(Xeption exception)
+        {
+            var videoMetadataDependencyException =
+                new VideoMetadataDependencyException(
+                    "Video Metadata dependency exception error occured, please contact support.",
+                        exception);
+
+            this.loggingBroker.LogCritical(videoMetadataDependencyException);
+
+            return videoMetadataDependencyException;
         }
 
         private VideoMetadataValidationException CreateAndLogValidationException(Xeption exception)
