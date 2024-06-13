bb0082386a1c93a9855ef7d1da289ebf387464a6
Abdurahmonov Azizbek <abdurahmonov-azizbek@outlook.com>
2024-06-13 00:19:46 -0700

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> FAIL

---
+++ VidSphere.Core.Api.Tests/Services/Foundations/VideoMetadatas/VideoMetadataServiceTests.Validations.RetrieveById.cs
@@ -0,0 +1,54 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by VidSphere Team
+// --------------------------------------------------------
+
+using FluentAssertions;
+using Moq;
+using VidSphere.Core.Api.Models.VideoMetadatas;
+using VidSphere.Core.Api.Models.VideoMetadatas.Exceptions;
+
+namespace VidSphere.Core.Api.Tests.Services.Foundations.VideoMetadatas
+{
+    public partial class VideoMetadataServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            //given
+            var invalidVideoMetadataId = Guid.Empty;
+            var invalidVideoMetadataException = new InvalidVideoMetadataException(
+                message: "Video Metadata is invalid.");
+
+            invalidVideoMetadataException.AddData(
+                key: nameof(VideoMetadata.Id),
+                values: "Id is required");
+
+            var expectedVideoMetadataValidationException = new VideoMetadataValidationException(
+                message: "Video Metadata Validation Exception occured, fix the errors and try again.",
+                innerException: invalidVideoMetadataException);
+
+            //when
+            ValueTask<VideoMetadata> retrieveVideoMetadataByIdTask =
+                this.videoMetadataService.RetrieveVideoMetadataByIdAsync(invalidVideoMetadataId);
+
+            VideoMetadataValidationException actuallVideoMetadataValidationException =
+                await Assert.ThrowsAsync<VideoMetadataValidationException>(
+                    retrieveVideoMetadataByIdTask.AsTask);
+
+            //then
+            actuallVideoMetadataValidationException.Should().BeEquivalentTo(expectedVideoMetadataValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedVideoMetadataValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectVideoMetadataByIdAsync(It.IsAny<Guid>()), Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
