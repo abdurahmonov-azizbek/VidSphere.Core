72368d2a626f3a7050f9e74381fb97b2f6dcfe5f
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-06-15 18:52:32 +0500

    CONFIG: Configure MiddleWare

--- VidSphere.Core.Api/Program.cs
+++ VidSphere.Core.Api/Program.cs
@@ -3,18 +3,23 @@
 // Developed by VidSphere Team
 // --------------------------------------------------------
 
+using VidSphere.Core.Api.Brokers.DateTimes;
+using VidSphere.Core.Api.Brokers.Loggings;
+using VidSphere.Core.Api.Brokers.Storages;
+using VidSphere.Core.Api.Services.Foundations.VideoMetadatas;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IStorageBroker, StorageBroker>();
+builder.Services.AddTransient<ILoggingBroker, LoggingBroker>();
+builder.Services.AddTransient<IDateTimebroker, DateTimeBroker>();
+builder.Services.AddTransient<IVideoMetadataService, VideoMetadataService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
