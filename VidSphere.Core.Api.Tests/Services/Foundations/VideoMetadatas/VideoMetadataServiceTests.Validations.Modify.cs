3079c4b543426c803e5c378c60c0b06b1f77885b
UmidjonDeveloperr <umidjonshonazarov895@gmail.com>
2024-06-15 17:07:07 +0500

    ShouldThrowValidationExceptionOnModifyIfVideoMetadataIsNullAndLogItAsync -> FAIL

---
+++ VidSphere.Core.Api.Tests/Services/Foundations/VideoMetadatas/VideoMetadataServiceTests.Validations.Modify.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VidSphere.Core.Api.Models.VideoMetadatas.Exceptions;
+using VidSphere.Core.Api.Models.VideoMetadatas;
+using FluentAssertions;
+
+namespace VidSphere.Core.Api.Tests.Services.Foundations.VideoMetadatas
+{
+    public partial class VideoMetadataServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfVideoMetadataIsNullAndLogItAsync()
+        {
+            // given
+            VideoMetadata nullVideoMetadata = null;
+            var nullVideoMetadataException = new NullVideoMetadataException("Video Metadata is null.");
+
+            var expectedVideoMetadataValidationException =
+                new VideoMetadataValidationException(
+                    message: "Video Metadata Validation Exception occured, fix the errors and try again.",
+                    innerException: nullVideoMetadataException);
+
+            // when
+            ValueTask<VideoMetadata> modifyVideoMetadataTask =
+                this.videoMetadataService.ModifyVideoMetadataAsync(nullVideoMetadata);
+
+            VideoMetadataValidationException actualVideoMetadataValidationException =
+                await Assert.ThrowsAsync<VideoMetadataValidationException>(
+                    modifyVideoMetadataTask.AsTask);
+
+            // then
+            actualVideoMetadataValidationException.Should().BeEquivalentTo(
+                expectedVideoMetadataValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(expectedVideoMetadataValidationException))),
+                    Times.Once());
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
